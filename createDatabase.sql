4b62f9fafa4966253ce90e06c8e4d1924d60c614
Pedro Leiva <pedrolei@usc.edu>
2017-11-11 16:05:51 -0800

    Pushing database creation Script

---
+++ createDatabase.sql
@@ -0,0 +1,15 @@
+	Drop DATABASE if exists PlayerBase;
+    CREATE DATABASE PlayerBase;
+    USE PlayerBase;
+    
+    CREATE TABLE Player (
+		username VARCHAR(20) primary key not null,
+        password VARCHAR(20) not null,
+		levelcompleted int(10) not null
+    );
+    
+    INSERT INTO Player (username, password, levelcompleted) VALUES (johnny, karate, 2);
+    INSERT INTO Player (username, password, levelcompleted) VALUES (me, me, 1);
+    
+    
+	
